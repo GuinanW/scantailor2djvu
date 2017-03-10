157d6285dcdaf893dcc34fc1b8eed225d048420a
Guinan <cool.guinan@yandex.ru>
2017-03-10 15:54:10 +0300

    fr/get_txt_from_fr_djvu.sh - корректировка номеров страниц при извлечении текста из FR

--- fr/get_txt_from_fr_djvu.sh
+++ fr/get_txt_from_fr_djvu.sh
@@ -7,7 +7,10 @@ in_djvu="fr.djvu"
 mkdir txtz txt
 pages=`djvused "${in_djvu}" -e 'n'`
 
-for pn in `seq -w 1 ${pages}`;do
-    djvuextract "${in_djvu}" -page=${pn} TXTz="txtz/${pn}.txtz"
-    djvused "${in_djvu}" -u -e "select ${pn};print-txt" > "txt/${pn}.txt"
+diff_num=1 #разница между номером страницы в FR и необходимым
+
+for out_num in `seq -w 0000 ${pages}`;do #выходные номера
+    pn=`expr ${out_num} + ${diff_num}` #номер страницы в документе fr
+    djvuextract "${in_djvu}" -page=${pn} TXTz="txtz/${out_num}.txtz"
+    djvused "${in_djvu}" -u -e "select ${pn};print-txt" > "txt/${out_num}.txt"
 done
