ef46df7274e79423538f46033ad78f4a149510e7
Guinan <cool.guinan@yandex.ru>
2017-05-05 21:03:41 +0300

    djvu/_archive.sh - добавил подсчёт md5sum

--- djvu/_archive.sh
+++ djvu/_archive.sh
@@ -3,7 +3,10 @@
 
 #придумываем пароль
 if [ \! -f pass ]; then
-    pwqgen | tr -d '\n' > pass
+    echo -n 'book_password' > pass
+    if [ -x /usr/bin/pwqgen ]; then
+        pwqgen | tr -d '\n' > pass
+    fi
 fi
 
 pass=`cat pass`
@@ -16,15 +19,21 @@ out_dir="archives"
 mkdir "${out_dir}"
 
 #сырые сканы
-#7z a -mhe=on -p"${pass}" ${volume_size} "${out_dir}/${name}_raw.7z" *.tif #*.ScanTailor
-#par2 c "${out_dir}/${name}_raw.par2" "${out_dir}/${name}_raw.7z".* #избыточность для восстановления
+7z a -mhe=on -p"${pass}" ${volume_size} "${out_dir}/${name}_raw.7z" *.tif #*.ScanTailor
+par2 c "${out_dir}/${name}_raw.par2" "${out_dir}/${name}_raw.7z".* #избыточность для восстановления
 
 #вручную обработанные
 #7z a -mhe=on -p"${pass}" ${volume_size} "${out_dir}/${name}_manual.7z" *.ScanTailor out/export/{img_clean,img_black,cover,img_clean/gray}/*.{tif,xcf} out/export/mark/*.png djvu/ fr/ on_black/
 7z a -mhe=on -p"${pass}" ${volume_size} "${out_dir}/${name}_manual.7z" *.ScanTailor out/export/ djvu/ fr/
+
+pushd "${out_dir}"
+md5sum *.* > md5sums
+popd
+
 popd
 
 #результат
 pushd "../_out/"
+echo
 rar a -hp"${pass}" -r "../${out_dir}/${name}.rar" .
 popd
