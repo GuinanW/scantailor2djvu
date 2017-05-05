ec4a3ae00bcf8e6c0f15b7e9fc953330ccc6bffd
Guinan <cool.guinan@yandex.ru>
2017-05-05 20:42:28 +0300

    out/_split.sh - разделение страниц с возможностью перенумерации

--- out/_split.sh
+++ out/_split.sh
@@ -3,7 +3,12 @@
 
 mkdir -p "export/1" "export/2"
 
+i=1 #счётчик номера страницы
+
 for f in *.tif;do
-    convert "${f}" -threshold 1 "export/1/${f}"
-    convert "${f}" -mask "export/1/${f}" -fill white -opaque black +mask "export/2/${f}"
+    #of="${f}" #с оригинальными именами
+    of=`printf "%04d.tif" ${i}` #перенумеровываем файлы по порядку в вид "0000.tif"
+    convert "${f}" -threshold 1 -monochrome -compress lzw "export/1/${of}"
+    convert "${f}" -mask "export/1/${of}" -fill white -opaque black +mask -compress lzw "export/2/${of}"
+    i=`expr ${i} + 1`
 done
