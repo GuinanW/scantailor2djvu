6a9d28ee7556f424ac996c521e457a3ce1923fa5
Guinan <cool.guinan@yandex.ru>
2017-05-05 20:56:36 +0300

    out/export/2/mv_pics_img.sh - по умолчанию медленно считается количество цветов

--- out/export/2/mv_pics_img.sh
+++ out/export/2/mv_pics_img.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 # перемещаем файлы с нормальной графикой, а не номером страницы
 
-mkdir imgs
-#значение хеша для пустой страницы
-null_hash="9ff52caefd9026181cba529c7e9be0a9"
+#если пустой, то медленно отделяем страницы всего с одним цветом
+null_hash=""
 
+#значение хеша для пустой страницы
+#null_hash="49912f8d859fddff6ce8aafe94de3b07"
+
+mkdir imgs
 for f in *.tif;do
     echo "${f}"
     #hash=`convert "${f}" -crop 3660x5047+0+300 - | identify -format '%#' -` #обрезаем часть страницы с номером или ещё чем
     #hash=`convert "${f}" - | identify -format '%#' -`
-    hash=`md5sum "${f}" | cut -f1 -d ' '`
-    [ "$hash" == "$null_hash" ] && continue
-    #tmp_ppm=`mktemp --suffix '.ppm'`
+    if [ -z "${null_hash}" ];then
+        #если цвет только один, то считаем что страница пустая
+        num_color=`identify -format '%k' "${f}"`
+        [ $num_color -eq 1 ] && continue
+    else
+        hash=`md5sum "${f}" | cut -f1 -d ' '`
+        [ "$hash" == "$null_hash" ] && continue
+    fi
+    #если пустые файлы имеют разные хеши
+    #[ "$hash" == "49912f8d859fddff6ce8aafe94de3b07" ] && continue
 
     mv -v "${f}" imgs/
 done
